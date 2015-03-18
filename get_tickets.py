6dba85d6f52b0e88056b5fe868eecffa53560e3b
Angelica Coleman <angelicacoleman3@gmail.com>
2015-03-18 12:00:58 -0700

    updating filters

--- get_tickets.py
+++ get_tickets.py
@@ -1,8 +1,13 @@
 import requests
 
 url = 'https://{subdomain}.zendesk.com/api/v2/tickets.json'
+<<<<<<< HEAD
 user = '{email}'
 pwd = '{password}'
+=======
+user = 'email'
+pwd = 'password'
+>>>>>>> 5989b90bb202555f07c48b78669ba7974f3c7a0d
 
 response = requests.get(url, auth=(user, pwd))
 
@@ -12,6 +17,7 @@ if response.status_code != 200:
 
 data = response.json()
 
+<<<<<<< HEAD
 ticket_list = data['tickets']
 tickets_by_requester =[]
 for ticket in ticket_list:
@@ -24,3 +30,10 @@ print tickets_by_requester
 
 
 
+=======
+#create any sort methods below
+ticket_list = data['tickets']
+for ticket in ticket_list:
+	if ticket['status'] == 'pending':
+		print(ticket['id'], ticket['status'], ticket['type'], ticket['priority'] , ticket['requester_id'], ticket['submitter_id'], ticket['organization_id'])
+>>>>>>> 5989b90bb202555f07c48b78669ba7974f3c7a0d
